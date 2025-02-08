f37b54920f5b6eb6e4034b412cece1ee8c555c5c
asmatar <deruelle.arthur@gmail.com>
2025-02-08 21:18:42 +0100

    stripe without webhook

--- components/checkout/StripeProvider.tsx
+++ components/checkout/StripeProvider.tsx
@@ -4,9 +4,63 @@ import {
   useStripe,
 } from "@stripe/react-stripe-js";
 import { useRouter } from "next/navigation";
+import { useFormStatus } from "react-dom";
 import { Button } from "../ui/button";
 import { Separator } from "../ui/separator";
 
+function Submit() {
+  const { pending } = useFormStatus();
+  const stripe = useStripe();
+  const elements = useElements();
+  return (
+    <>
+      <Button
+        disabled={!stripe || !elements || pending}
+        id="submit"
+        type="submit"
+        className="mt-4"
+        variant="default"
+      >
+        <span id="button-text">{pending ? "Processing..." : "Pay Now"}</span>
+      </Button>
+    </>
+  );
+}
+function Form({
+  action,
+  startDate,
+  endDate,
+  totalPrice,
+  breakfastPrice,
+  breakfastIncluded,
+}: {
+  action: any;
+  startDate: string;
+  endDate: string;
+  totalPrice: number;
+  breakfastPrice: number;
+  breakfastIncluded: boolean;
+}) {
+  return (
+    <form action={action}>
+      <PaymentElement id="payment-element" options={{ layout: "tabs" }} />
+      <div className="flex flex-col gap-1 mt-4">
+        <h2 className="text-lg mb-1 font-semibold">Your booking summary</h2>
+        <div className="">you will check in on {startDate} at 5PM</div>
+        <div className="">you will check out on {endDate} at 11PM</div>
+        {breakfastIncluded && (
+          <div className="">you will be served breakfast each day at 8PM</div>
+        )}
+        <Separator />
+        <div className="font-bold text-lg ">
+          <div className="mb-2">Breakfast Price: ${breakfastPrice}</div>
+          total price: ${totalPrice}
+        </div>
+      </div>
+      <Submit />
+    </form>
+  );
+}
 function StripeProvider({
   startDate,
   endDate,
@@ -23,10 +77,7 @@ function StripeProvider({
   const router = useRouter();
   const stripe = useStripe();
   const elements = useElements();
-  //const { clientSecret } = useBookingStore();
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
+  const handleSubmit = async () => {
     if (!stripe || !elements) {
       return;
     }
@@ -54,31 +105,14 @@ function StripeProvider({
 
   return (
     <>
-      <form id="payment-form" onSubmit={handleSubmit}>
-        <PaymentElement id="payment-element" options={{ layout: "tabs" }} />
-        <div className="flex flex-col gap-1 mt-4">
-          <h2 className="text-lg mb-1 font-semibold">Your booking summary</h2>
-          <div className="">you will check in on {startDate} at 5PM</div>
-          <div className="">you will check out on {endDate} at 11PM</div>
-          {breakfastIncluded && (
-            <div className="">you will be served breakfast each day at 8PM</div>
-          )}
-          <Separator />
-          <div className="font-bold text-lg ">
-            <div className="mb-2">Breakfast Price: ${breakfastPrice}</div>
-            total price: ${totalPrice}
-          </div>
-        </div>
-        <Button
-          disabled={!stripe || !elements}
-          id="submit"
-          type="submit"
-          className="mt-4"
-          variant="default"
-        >
-          <span id="button-text">Pay now</span>
-        </Button>
-      </form>
+      <Form
+        action={handleSubmit}
+        startDate={startDate}
+        endDate={endDate}
+        totalPrice={totalPrice}
+        breakfastPrice={breakfastPrice}
+        breakfastIncluded={breakfastIncluded}
+      />
     </>
   );
 }
