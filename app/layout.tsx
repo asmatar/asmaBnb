5f91ade6dc237d30e1be7ea244d0cd45707f59f6
asmatar <deruelle.arthur@gmail.com>
2025-04-21 08:57:37 +0200

    setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,13 @@ import Header from "@/components/Header";
 import { ThemeProvider } from "@/components/theme-provider";
 import Container from "@/components/ui/Container";
 import { cn } from "@/lib/utils";
-
 import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
+import { NextIntlClientProvider, hasLocale } from "next-intl";
 import { Roboto } from "next/font/google";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import routing from "../i18n/i18n";
 import "./globals.css";
 const roboto = Roboto({ weight: ["400", "700"], subsets: ["latin"] });
 
@@ -20,36 +21,44 @@ export const metadata: Metadata = {
     "Découvrez Asma Hotel, votre destination de luxe pour des séjours inoubliables. Profitez de nos chambres élégantes, installations modernes et service exceptionnel pour une expérience hôtelière parfaite.",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
+  params,
 }: Readonly<{
   children: React.ReactNode;
+  params: { locale: string };
 }>) {
+  const { locale } = await params;
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
   return (
     <ClerkProvider>
-      <html lang="en" suppressHydrationWarning>
+      <html lang={locale} suppressHydrationWarning>
         <body className={cn(roboto.className)}>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <Header />
-            <Container>{children}</Container>
-            <ToastContainer
-              position="bottom-right"
-              autoClose={1000}
-              hideProgressBar={false}
-              newestOnTop
-              closeOnClick
-              pauseOnFocusLoss
-              draggable
-              pauseOnHover
-              theme="light"
-              limit={3}
-            />
-          </ThemeProvider>
+          <NextIntlClientProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
+              <Header />
+              <Container>{children}</Container>
+              <ToastContainer
+                position="bottom-right"
+                autoClose={1000}
+                hideProgressBar={false}
+                newestOnTop
+                closeOnClick
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+                limit={3}
+              />
+            </ThemeProvider>
+          </NextIntlClientProvider>
         </body>
       </html>
     </ClerkProvider>
