fd46b9630766e82b51b7478ba0b0146a8cb0868c
asmatar <deruelle.arthur@gmail.com>
2025-11-25 19:35:24 +0100

    favorite design + toast

--- components/Favorite.tsx
+++ components/Favorite.tsx
@@ -1,6 +1,7 @@
 import { useUser } from "@clerk/nextjs";
 import { Heart } from "lucide-react";
 import { useTranslations } from "next-intl";
+import { toast } from "react-toastify";
 import IconCTA from "./IconCTA";
 import {
   Tooltip,
@@ -12,6 +13,7 @@ import {
 const Favorites = ({ isFavorite, id }: { isFavorite: boolean; id: string }) => {
   const { user } = useUser();
   const t = useTranslations("HotelCard");
+
   return (
     <>
       {user ? (
@@ -38,15 +40,19 @@ const Favorites = ({ isFavorite, id }: { isFavorite: boolean; id: string }) => {
         <TooltipProvider>
           <Tooltip>
             <TooltipTrigger asChild>
-              <p className="text-sm text-muted-foreground line-clamp-2 cursor-help">
-                <span className="bg-background/80 backdrop-blur-sm p-2 rounded-full shadow-sm transition-transform hover:scale-105">
-                  <Heart
-                    className="w-5 h-5 stroke-rose-500 hover:fill-rose-500/20 transition-colors"
-                    fill="none"
-                    strokeWidth={2}
-                  />
-                </span>
-              </p>
+              <button
+                className="bg-background rounded-full shadow-sm transition-transform hover:scale-105 z-50 p-1"
+                onClick={(event) => {
+                  event.preventDefault();
+                  toast.error(t("favorites.isFavoriteAllowed"));
+                }}
+              >
+                <Heart
+                  className="w-5 h-5 stroke-rose-500 hover:fill-rose-500/20 transition-colors"
+                  fill="none"
+                  strokeWidth={2}
+                />
+              </button>
             </TooltipTrigger>
             <TooltipContent className="w-fit p-3">
               {t("favorites.isFavoriteAllowed")}
